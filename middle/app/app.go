7956605e42d8198cb6c1b567a355055e650637aa
Artsiom Kaliaha <stakkato95@gmail.com>
2022-06-04 15:04:45 +0200

    Gin. Add middleware

--- middle/app/app.go
+++ middle/app/app.go
@@ -1,14 +1,51 @@
 package app
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/stakkato95/service-engineering-go-lib/logger"
 	"github.com/stakkato95/service-engineering-microservice-infrastructure/middle/config"
 )
+
+const XHeadersKey = "x-headers"
+
+func XHeadersPropagation() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		headers := http.Header{}
+
+		for key, val := range ctx.Request.Header {
+			if strings.ToLower(key)[0] == 'x' {
+				headers.Set(key, val[0])
+			}
+		}
+
+		ctx.Set(XHeadersKey, headers)
+		ctx.Next()
+	}
+}
+
+func GetXHeaders(ctx *gin.Context) http.Header {
+	headersRaw, ok := ctx.Get(XHeadersKey)
+	if !ok {
+		logger.Fatal("XHeadersPropagation is not used")
+	}
+	headers, ok := headersRaw.(http.Header)
+	if !ok {
+		logger.Fatal("XHeadersPropagation cast error")
+	}
+	return headers
+}
 
 func Start() {
 	h := Handler{}
 
-	router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	router.Use(XHeadersPropagation())
+
 	router.GET("/request", h.getRequest)
 	router.Run(config.AppConfig.ServerPort)
 }
--- middle/app/handler.go
+++ middle/app/handler.go
@@ -2,62 +2,24 @@ package app
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/stakkato95/service-engineering-go-lib/logger"
 	"github.com/stakkato95/service-engineering-microservice-infrastructure/middle/dto"
 )
 
 type Handler struct {
 }
 
-type telemetryHeaders struct {
-	X_request_id      string `header:"x-request-id"`
-	X_b3_traceid      string `header:"x-b3-traceid"`
-	X_b3_spanid       string `header:"x-b3-spanid"`
-	X_b3_parentspanid string `header:"x-b3-parentspanid"`
-	X_b3_sampled      string `header:"x-b3-sampled"`
-	X_b3_flags        string `header:"x-b3-flags"`
-	B3                string `header:"b3"`
-}
-
-func (h *telemetryHeaders) ToHeaders() http.Header {
-	return http.Header{
-		"x-request-id":      {h.X_request_id},
-		"x-b3-traceid":      {h.X_b3_traceid},
-		"x-b3-spanid":       {h.X_b3_spanid},
-		"x-b3-parentspanid": {h.X_b3_parentspanid},
-		"x-b3-sampled":      {h.X_b3_sampled},
-		"x-b3-flags":        {h.X_b3_flags},
-		"b3":                {h.B3},
-	}
-}
-
 func (h *Handler) getRequest(ctx *gin.Context) {
-	headers := http.Header{}
-
-	for key, val := range ctx.Request.Header {
-		if strings.ToLower(key)[0] == 'x' {
-			headers.Set(key, val[0])
-		}
-	}
-	logger.Info(fmt.Sprintf("%v", headers))
-
-	tHeaders := telemetryHeaders{}
-
-	if err := ctx.ShouldBindHeader(&tHeaders); err != nil {
-		ctx.JSON(http.StatusOK, err)
-	}
+	xheaders := GetXHeaders(ctx)
 
 	req, err := http.NewRequest("GET", "http://backend-1/request", nil)
 	if err != nil {
 		errorResponse(ctx, err)
 		return
 	}
-	req.Header = headers
+	req.Header = xheaders
 
 	var res *http.Response
 	res, err = http.DefaultClient.Do(req)
@@ -74,7 +36,7 @@ func (h *Handler) getRequest(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, dto.ServiceResponseDto{
 		Service:      "middle",
-		X_request_id: tHeaders.X_request_id,
+		X_request_id: xheaders["X-Request-Id"][0],
 		Nested:       nestedResponseDto,
 	})
 }
